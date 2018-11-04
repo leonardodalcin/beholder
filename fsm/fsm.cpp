21937804c275e71f7896445c4d71596f07957951
Luis Felipe <silva.luisfelipe13@gmail.com>
2018-11-04 21:49:20 -0200

    Added states

---
+++ fsm/fsm.cpp
@@ -0,0 +1,103 @@
+#include "fms.hpp"
+
+fms::fms(boost::asio::io_service& io) : _timer(io),
+					_state(state::end) {
+    change_state(state::start);
+}
+
+void fsm::change_state(state new_state) {
+    std::cout << "Changing state from \"" << _state << "\" to \"" << new_state << '\"';
+
+    // On State Exit
+    switch (_state) {
+	case state::start:
+	    exit_start();
+	    break;
+	case state::wait_open:
+	    exit_wait_open();
+	    break;
+	case state::classify_cable_tie_length:
+	    exit_classify_cable_tie_length();
+	    break;
+	case state::wait_ejection:
+	    exit_wait_ejection();
+	    break;
+	case state::classify_ejection:
+	    exit_classify_ejection();
+	    break;
+	case state::end:
+	    exit_end();
+	    break;
+    }
+
+    auto old_state = _state;
+    _state = new_state;
+
+    // On State Entry
+    switch (_state) {
+	case state::start:
+	    enter_start();
+	    break;
+	case state::wait_open:
+	    enter_wait_open();
+	    break;
+	case state::classify_cable_tie_length:
+	    enter_classify_cable_tie_length();
+	    break;
+	case state::wait_ejection:
+	    enter_wait_ejection();
+	    break;
+	case state::classify_ejection:
+	    enter_classify_ejection();
+	    break;
+	case state::end:
+	    enter_end();
+	    break;
+    }
+}
+
+// exit
+void fsm::exit_start() {
+}
+
+void fsm::exit_wait_open() {
+}
+
+void fsm::exit_classify_cable_tie_length() {
+}
+
+void fsm::exit_wait_ejection() {
+}
+
+void fsm::exit_classify_ejection() {
+}
+
+void fsm::exit_end() {
+    // preparar sistema
+}
+
+// enter
+void fsm::enter_start() {
+    // preparar sistema
+    change_state(state::wait_open);
+}
+
+void fsm::enter_wait_open() {
+    change_state(state::classify_cable_tie_length);
+}
+
+void fsm::enter_classify_cable_tie_length() {
+    change_state(state::wait_ejection);
+}
+
+void fsm::enter_wait_ejection() {
+    change_state(state::classify_ejection);
+}
+
+void fsm::enter_classify_ejection() {
+    change_state(state::wait_open);
+}
+
+void fsm::enter_end() {
+    // finalizar pendencias
+}
