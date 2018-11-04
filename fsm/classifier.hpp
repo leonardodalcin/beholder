d8fcbe86c47a3c919f6a1770e8e1b0b6f6da579b
Luis Felipe <silva.luisfelipe13@gmail.com>
2018-11-04 21:58:32 -0200

    Added classifier mockup

---
+++ fsm/classifier.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+class classifier {
+   public:
+    classifier(/*opencv*/);
+
+    bool classify_cable_tie_length();
+    bool classify_ejection();
+
+   private:
+    // _opencv
+};
